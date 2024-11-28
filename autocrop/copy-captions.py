20a3c87b8ae26fc2a2c942e22c0bfb52d13818e5
Howard Lam <puiholam2-c@my.cityu.edu.hk>
2024-11-28 00:21:35 +0000

    copy caption if not exists in out

--- autocrop/copy-captions.py
+++ autocrop/copy-captions.py
@@ -4,19 +4,17 @@ import sys
 
 if __name__ == '__main__':
     in_dir, out_dir = sys.argv[1], sys.argv[2]
-    for root, _, files in os.walk(in_dir):
+    out_set = []
+    for root, _, files in os.walk(out_dir):
         for file in files:
             if file.endswith('.txt'):
                 continue
-            img_rel_path = os.path.relpath(os.path.join(root,file), in_dir)
-            out_img_path = os.path.join(out_dir, img_rel_path)
-            if not os.path.exists(out_img_path):
-                continue
+            img_rel_path = os.path.relpath(os.path.join(root,file), out_dir)
             fn, _ = os.path.splitext(file)
-            cap_file = os.path.join(root, fn+'.txt')
-            if os.path.exists(cap_file):
-                cap_rel_path = os.path.relpath(cap_file, in_dir)
-                target = os.path.join(out_dir, cap_rel_path)
-                tar_root, _ = os.path.split(target)
-                os.makedirs(tar_root, exist_ok=True)
-                shutil.copyfile(cap_file, target)
+            if not os.path.exists(os.path.join(root,fn+'.txt')):
+                cap_rel_path, _ = os.path.splitext(img_rel_path)
+                cap_rel_path += '.txt'
+                src = os.path.join(in_dir, cap_rel_path)
+                tar = os.path.join(out_dir, cap_rel_path)
+                if os.path.exists(src):
+                    shutil.copyfile(src, tar)
